87ab11c71afa8afc35da0d6b72fa455037f6164d
ozgurdinc <ozgur.dinc@std.yeditepe.edu.tr>
2026-01-06 15:27:09 +0300

    Update TaskListScreen.kt
    tasklist

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -7,27 +7,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.yeditepe.finalexam.model.Task
 import com.yeditepe.finalexam.viewmodel.TaskViewModel
 
 @Composable
-fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
-
-    // TODO 3: Read task list from ViewModel
-
+fun TaskListScreen(
+    viewModel: TaskViewModel = viewModel(),
+    navController: NavHostController,
+    task: Task
+) {
+    val tasks = viewModel.tasks
     Column {
-        // TODO 4: Display task titles and completion status
-        // Use a simple Column or LazyColumn
+            TaskRow(task = task, navController = navController)
+        }
     }
 }
-
 @Composable
 fun TaskRow(task: Task, navController: NavController) {
-
     Text(
         text = task.title,
         modifier = Modifier.clickable {
-            // TODO 3: Navigate to detail screen with task title
+            navController.navigate("taskDetail/${task.title}")
         }
     )
 }
+
